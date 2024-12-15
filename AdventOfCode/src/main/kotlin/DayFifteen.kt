b2e3c3f2ea365c419709f87245c121dcd3c6277b
BnjmnCummings <cummingsben097@gmail.com>
2024-12-15 16:14:26 +0000

    Day 15 part 1

---
+++ AdventOfCode/src/main/kotlin/DayFifteen.kt
@@ -0,0 +1,162 @@
+import java.io.File
+
+val GRID_MATCHER = Regex("#.+#")
+val INST_MATCHER = Regex("[\\^v><]+")
+
+/**
+ * Represents the grid and instruction set of the robot.
+ */
+class QuestionFifteenInput(
+    val grid: MutableList<MutableList<Char>>,
+    val instructions: MutableList<Char>,
+)
+
+/**
+ * Collects the grid and instruction set for question 5.
+ * @param filename the path to the desired file
+ */
+fun questionFifteen(filename: String):QuestionFifteenInput {
+    val input = QuestionFifteenInput(ArrayList(), ArrayList())
+    File(filename).forEachLine {
+        if(GRID_MATCHER.matches(it)) {
+            input.grid.add( it.toMutableList())
+        } else if(INST_MATCHER.matches(it)) {
+            input.instructions.addAll(it.toList())
+        }
+    }
+    return input
+}
+
+/**
+ * Calculates the sum of all the boxes coordinates of the sorted unit.
+ * using calculateCoordinate()
+ * @param grid the current state of the grid
+ */
+fun sumBoxCoordinates(grid: List<List<Char>>):Int {
+    var total = 0
+    fun calculateCoordinate(row:Int, col:Int):Int = (100 * row) + col
+
+    for(i in grid.indices) {
+        for(j in grid.first().indices) {
+            if(grid[i][j] == 'O') {
+                total += calculateCoordinate(i, j)
+            }
+        }
+    }
+    return total
+}
+
+/**
+ * Finds and returns the coordinates of the robot.
+ * To be used at the start of the simulation.
+ * @param grid the current state of the grid
+ * @return coordinates of the robot, null if not found
+ */
+fun getRobotCoordinates(grid: List<List<Char>>):Pair<Int, Int>? {
+    for(i in grid.indices) {
+        for(j in grid.first().indices) {
+            if(grid[i][j] == '@') {
+                return Pair(i, j)
+            }
+        }
+    }
+    return null
+}
+
+/**
+ * Moves a block/robot by recursively calling itself on the desired location
+ * @param row the current row of the object
+ * @param col the current row of the object
+ * @param grid the current state of the grid
+ * @param direction the direction of travel using the Direction enum from day 6
+ * @return true if it's happened successfully
+ */
+fun moveCharacter(row:Int, col:Int, direction: Direction, grid: List<MutableList<Char>>):Boolean {
+    /* base case: we return true if the there is an empty space and false if there's a wall
+    in each case, nothing moves */
+    if(grid[row][col] == '.') return true
+    if(grid[row][col] == '#') return false
+
+    /* we don't need t consider index out of bounds errors as all the edge cases contain '#' characters*/
+    val (newRow, newCol) = moveOneSquare(row, col, direction)
+
+    /* we update the grid and return true if we can call moveCharacter() on our new row and col.
+    This will only update if the base case returns true, i.e. there is an empty space available */
+    if(moveCharacter(newRow, newCol, direction, grid)) {
+        grid[newRow][newCol] = grid[row][col]
+        grid[row][col] = '.'
+        return true
+    }
+    return false
+}
+
+/**
+ * Moves the robot according to given instructions, updating the grid for each one
+ * @param startRow the current row of the object
+ * @param startCol the current row of the object
+ * @param input the current state of the grid
+ */
+fun simulatePacking(startRow:Int, startCol:Int, input: QuestionFifteenInput) {
+    var row = startRow
+    var col = startCol
+    val grid = input.grid
+    val instructions = input.instructions
+
+    for(char in instructions) {
+        val direction = when(char) {
+            '^'   -> Direction.UP
+            '>'   -> Direction.RIGHT
+            'v'   -> Direction.DOWN
+            '<'   -> Direction.LEFT
+            else -> null
+        }
+
+        /* move our robot if we can */
+        if(moveCharacter(row, col, direction!!, grid)) {
+            val (newRow, newCol) = moveOneSquare(row, col, direction)
+            row = newRow
+            col = newCol
+        }
+    }
+}
+
+/**
+ * Helper function to move the robot by one square depending on the direction.
+ * Doesn't handle index out of bounds errors.
+ * @param row the current row of the object
+ * @param col the current row of the object
+ * @param direction the direction of travel using the Direction enum from day 6
+ */
+fun moveOneSquare(row:Int, col:Int, direction: Direction):Pair<Int, Int> =
+    when (direction) {
+        Direction.UP    -> row - 1 to col
+        Direction.RIGHT -> row     to col + 1
+        Direction.DOWN  -> row + 1 to col
+        Direction.LEFT  -> row     to col - 1
+    }
+
+/* Part 2 */
+
+fun widenGrid(grid:List<MutableList<Char>>):List<MutableList<Char>> =
+    grid.map {it.flatMap { c ->
+        when(c) {
+            '#' -> "##".toList()
+            '@' -> "@.".toList()
+            'O' -> "[]".toList()
+            else -> "..".toList()
+        }
+    }.toMutableList()
+    }
+
+
+fun main() {
+    val input = questionFifteen("src/main/resources/DayFifteen.txt")
+    val startingPos = getRobotCoordinates(input.grid)!!
+
+    simulatePacking(
+        startingPos.first,
+        startingPos.second,
+        input)
+
+    println(sumBoxCoordinates(input.grid))
+}
---
+++ AdventOfCode/src/test/kotlin/DayFifteenTest.kt
@@ -0,0 +1,281 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class DayFifteenTest {
+    @Test
+    fun `gets correct input`() {
+        val dummyGrid = listOf("##########" ,
+            "#..O..O.O#" ,
+            "#......O.#" ,
+            "#.OO..O.O#" ,
+            "#..O@..O.#" ,
+            "#O#..O...#" ,
+            "#O..O..O.#" ,
+            "#.OO.O.OO#" ,
+            "#....O...#" ,
+            "##########")
+
+        val dummyInstructions = "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^" +
+                "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v" +
+                "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<" +
+                "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^" +
+                "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><" +
+                "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^" +
+                ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^" +
+                "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>" +
+                "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>" +
+                "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+
+        val test = questionFifteen("src/test/resources/DayFifteenLarge.txt")
+
+        assertEquals(
+            dummyInstructions.toList(),
+            test.instructions
+        )
+
+        assertEquals(
+            dummyGrid.map { it.toList() },
+            test.grid
+        )
+        
+    }
+
+    @Test
+    fun `sumBoxCoordinates() correctly evaluates the coordinates`() {
+        val grid = listOf(
+            "##########" ,
+            "#.O.O.OOO#" ,
+            "#........#" ,
+            "#OO......#" ,
+            "#OO@.....#" ,
+            "#O#.....O#" ,
+            "#O.....OO#" ,
+            "#O.....OO#" ,
+            "#OO....OO#" ,
+            "##########")
+            .map { it.toList() }
+
+        assertEquals(
+            10092,
+            sumBoxCoordinates(grid)
+        )
+    }
+
+    @Test
+    fun `getRobotCoordinates() returns the position of the robot`() {
+        val grid = listOf(
+            "##########" ,
+            "#.O.O.OOO#" ,
+            "#........#" ,
+            "#OO......#" ,
+            "#OO@.....#" ,
+            "#O#.....O#" ,
+            "#O.....OO#" ,
+            "#O.....OO#" ,
+            "#OO....OO#" ,
+            "##########")
+            .map { it.toList() }
+
+        assertEquals(
+            Pair(4, 3),
+            getRobotCoordinates(grid)
+        )
+    }
+
+    @Test
+    fun `moveCharacter() moves a robot when there's nothing in the way`() {
+        val grid = listOf(
+            "##########" ,
+            "#.O.O.OOO#" ,
+            "#........#" ,
+            "#OO......#" ,
+            "#OO@.....#" ,
+            "#O#.....O#" ,
+            "#O.....OO#" ,
+            "#O.....OO#" ,
+            "#OO....OO#" ,
+            "##########")
+            .map { it.toMutableList() }
+
+        moveCharacter(4, 3, Direction.UP, grid)
+        assertEquals(
+            listOf(
+                "##########" ,
+                "#.O.O.OOO#" ,
+                "#........#" ,
+                "#OO@.....#" ,
+                "#OO......#" ,
+                "#O#.....O#" ,
+                "#O.....OO#" ,
+                "#O.....OO#" ,
+                "#OO....OO#" ,
+                "##########")
+                .map { it.toList() },
+
+            grid
+        )
+
+    }
+
+    @Test
+    fun `moveCharacter() does not move robot when there's a block`() {
+        val grid = listOf(
+            "##########" ,
+            "#.O.O.OOO#" ,
+            "#........#" ,
+            "#OO......#" ,
+            "#OO@.....#" ,
+            "#O#.....O#" ,
+            "#O.....OO#" ,
+            "#O.....OO#" ,
+            "#OO....OO#" ,
+            "##########")
+            .map { it.toMutableList() }
+
+        moveCharacter(4, 3, Direction.LEFT, grid)
+        assertEquals(
+            listOf(
+                "##########" ,
+                "#.O.O.OOO#" ,
+                "#........#" ,
+                "#OO......#" ,
+                "#OO@.....#" ,
+                "#O#.....O#" ,
+                "#O.....OO#" ,
+                "#O.....OO#" ,
+                "#OO....OO#" ,
+                "##########")
+                .map { it.toList() },
+
+            grid
+        )
+
+    }
+
+    @Test
+    fun `moveCharacter() allows robot to push one block`() {
+        val grid = listOf(
+            "##########" ,
+            "#.O.O.OOO#" ,
+            "#.@......#" ,
+            "#OO......#" ,
+            "#O.......#" ,
+            "#O......O#" ,
+            "#O.....OO#" ,
+            "#O.....OO#" ,
+            "#OO....OO#" ,
+            "##########")
+            .map { it.toMutableList() }
+
+        moveCharacter(2, 2, Direction.DOWN, grid)
+        assertEquals(
+            listOf(
+                "##########" ,
+                "#.O.O.OOO#" ,
+                "#........#" ,
+                "#O@......#" ,
+                "#OO......#" ,
+                "#O......O#" ,
+                "#O.....OO#" ,
+                "#O.....OO#" ,
+                "#OO....OO#" ,
+                "##########")
+                .map { it.toList() },
+
+            grid
+        )
+
+    }
+
+    @Test
+    fun `moveCharacter() allows robot to push many blocks`() {
+        val grid = listOf(
+            "##########" ,
+            "#.O.O.OOO#" ,
+            "#.@......#" ,
+            "#OO......#" ,
+            "#OO......#" ,
+            "#O......O#" ,
+            "#O.....OO#" ,
+            "#O.....OO#" ,
+            "#OO....OO#" ,
+            "##########")
+            .map { it.toMutableList() }
+
+        moveCharacter(2, 2, Direction.DOWN, grid)
+        assertEquals(
+            listOf(
+                "##########" ,
+                "#.O.O.OOO#" ,
+                "#........#" ,
+                "#O@......#" ,
+                "#OO......#" ,
+                "#OO.....O#" ,
+                "#O.....OO#" ,
+                "#O.....OO#" ,
+                "#OO....OO#" ,
+                "##########")
+                .map { it.toList() },
+
+            grid
+        )
+
+    }
+
+    @Test
+    fun `simulatePacking() correctly `() {
+        val input = questionFifteen("src/test/resources/DayFifteenSmall.txt")
+        val startingPos = getRobotCoordinates(input.grid)!!
+
+        simulatePacking(
+            startingPos.first,
+            startingPos.second,
+            input)
+
+        assertEquals(
+            listOf(
+                "########",
+                "#....OO#",
+                "##.....#",
+                "#.....O#",
+                "#.#O@..#",
+                "#...O..#",
+                "#...O..#",
+                "########")
+                .map { it.toList() },
+
+            input.grid
+        )
+    }
+
+    @Test
+    fun `widenGrid() replaces each square with one twice as wide` (){
+        val grid = listOf("##########" ,
+            "#..O..O.O#" ,
+            "#......O.#" ,
+            "#.OO..O.O#" ,
+            "#..O@..O.#" ,
+            "#O#..O...#" ,
+            "#O..O..O.#" ,
+            "#.OO.O.OO#" ,
+            "#....O...#" ,
+            "##########").map{ it.toMutableList() }
+        
+        assertEquals(
+            listOf(
+                "####################",
+                "##....[]....[]..[]##",
+                "##............[]..##",
+                "##..[][]....[]..[]##",
+                "##....[]@.....[]..##",
+                "##[]##....[]......##",
+                "##[]....[]....[]..##",
+                "##..[][]..[]..[][]##",
+                "##........[]......##",
+                "####################"
+            ).map { it.toMutableList()},
+            widenGrid(grid)
+        )
+
+    }
+}
